91e139fd9abeef5c5750615c8b48354928080347
Ryan Buchholtz <ryanscottbuchholtz@gmail.com>
2016-03-24 09:53:31 -0700

    js lightbox fix

--- js/main.js
+++ js/main.js
@@ -3,9 +3,3 @@ $(".box").mflip();
 $('.navigation a:lt(4)').click(function() {
     $(this).addClass("active").siblings().removeClass("active");
 });
-
-lightbox.option({
-      'resizeDuration': 200,
-      'wrapAround': true,
-      'maxWidth': 1000,
-    });
