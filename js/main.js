3ea6529191d67763c067e4b29423f6549c1528cb
Ryan Buchholtz <ryanscottbuchholtz@gmail.com>
2016-03-24 09:48:54 -0700

    change photo resolution

--- js/main.js
+++ js/main.js
@@ -3,3 +3,9 @@ $(".box").mflip();
 $('.navigation a:lt(4)').click(function() {
     $(this).addClass("active").siblings().removeClass("active");
 });
+
+lightbox.option({
+      'resizeDuration': 200,
+      'wrapAround': true,
+      'maxWidth': 1000,
+    });
